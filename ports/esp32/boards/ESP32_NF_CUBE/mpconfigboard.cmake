e01dde2a3de712cb123c935c4e5834093da7fa9f
Oscar Belter <oscar.belter@posteo.de>
2026-06-07 17:56:30 +0200

    Revert "removed the usb config changes"
    This reverts commit 3264efaf4f85e4da7cc280c3ca5a9f5eeef229ce.

--- ports/esp32/boards/ESP32_NF_CUBE/mpconfigboard.cmake
+++ ports/esp32/boards/ESP32_NF_CUBE/mpconfigboard.cmake
@@ -2,7 +2,7 @@ set(IDF_TARGET esp32s3)
 
 set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
-    # boards/ESP32_NF_CUBE/sdkconfig.usb
+    boards/ESP32_NF_CUBE/sdkconfig.usb
     boards/sdkconfig.ble
     boards/sdkconfig.spiram_sx
     boards/ESP32_GENERIC_S3/sdkconfig.board
